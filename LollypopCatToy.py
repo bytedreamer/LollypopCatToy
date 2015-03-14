23707850244253028d8779b3221e373cc1523403
Jonathan Horvath <byteme@bytedreamer.com>
2015-03-14 16:33:59 -0400

    Send unique queue status to each client

--- LollypopCatToy.py
+++ LollypopCatToy.py
@@ -1,7 +1,7 @@
 from flask import render_template
 from flask.ext.recaptcha import ReCaptcha
 from uuid import uuid4
-from application import create_app, add_to_queue
+from application import create_app, add_to_queue, socketio
 
 __author__ = 'Jonathan Horvath'
 
@@ -19,11 +19,18 @@ def register():
     if reCaptcha.verify():
         key = uuid4()
         add_to_queue(key)
-
         return render_template('register.html', key=key)
     else:
         return render_template('home.html')
 
 
+@socketio.on('connect', namespace='/queue')
+def queue_connect():
+    print('Client connected')
+
+@socketio.on('disconnect', namespace='/queue')
+def queue_disconnect():
+    print('Client disconnected')
+
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app)
--- application.py
+++ application.py
@@ -1,21 +1,25 @@
 import threading
 import atexit
 from flask import Flask
+from flask.ext.socketio import SocketIO
 
 __author__ = 'Jonathan Horvath'
 
-POOL_TIME = 5
+POLLING_TIME = 5
 PLAY_TIME = 60
 
 shared_queue = []
 time_left = 0
 data_lock = threading.Lock()
 tracking_thread = threading.Thread()
+socketio = SocketIO()
 
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_pyfile('config.py')
+    global socketio
+    socketio.init_app(app)
 
     def interrupt():
         global tracking_thread
@@ -28,20 +32,28 @@ def create_app():
         global data_lock
         with data_lock:
             if time_left <= 0 and len(shared_queue) >= 1:
-                print(shared_queue.pop())
+                key = shared_queue.pop()
+                socketio.emit(key, {'time_left': PLAY_TIME,
+                                    'queue_length': 0,
+                                    'is_ready': True}, namespace='/queue')
                 time_left = PLAY_TIME
             elif time_left > 0:
-                time_left -= POOL_TIME
+                time_left -= POLLING_TIME
 
-            print(time_left)
+            queue_length = 0
+            for key in reversed(shared_queue):
+                socketio.emit(key, {'time_left': (queue_length * PLAY_TIME) + time_left,
+                                    'queue_length': queue_length,
+                                    'is_ready': False}, namespace='/queue')
+                queue_length += 1
 
-        tracking_thread = threading.Timer(POOL_TIME, process_queue, ())
+        tracking_thread = threading.Timer(POLLING_TIME, process_queue, ())
         tracking_thread.start()
 
     def start():
         global tracking_thread
 
-        tracking_thread = threading.Timer(POOL_TIME, process_queue, ())
+        tracking_thread = threading.Timer(POLLING_TIME, process_queue, ())
         tracking_thread.start()
 
     start()
@@ -54,4 +66,3 @@ def add_to_queue(key):
     global data_lock
     with data_lock:
         shared_queue.insert(0, key)
-
