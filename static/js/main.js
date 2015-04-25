8a9fb40f18454e9267e502261b13508e7a4aebb8
bytedreamer <bytme@bytedreamer.com>
2015-04-25 13:50:53 -0400

    Fix framerate calculation and move javascript code to separate file.

--- static/js/main.js
+++ static/js/main.js
@@ -2,8 +2,45 @@
  * Created by jonathan on 3/7/15.
  */
 
-(function($){
+var CameraApp = function() {};
 
-    $(document).ready(function () {
+CameraApp.prototype = {
+    framesPerSecond: 1,
+
+    setup: function() {
+        this.image = document.getElementById('CameraImage');
+        this.imageSource = 'http://104.206.193.11:49709/dcnvr/video/live/image/2?w=320&h=240';
+        this.loadImage();
+    },
+
+    loadImage: function() {
+        var calculatedDelay = 1000 / this.framesPerSecond;
+        var currentTime = new Date().getTime();
+        if (this.lastLoadTime != undefined){
+            calculatedDelay = calculatedDelay - (currentTime - this.lastLoadTime);
+        }
+
+        this.lastLoadTime = currentTime;
+
+        if (calculatedDelay <= 0) {
+            this.image.src = this.imageSource + '&random=' + Math.random();
+        } else {
+            var cameraApp = this;
+            this.timer = setTimeout(function() {
+                cameraApp.image.src = cameraApp.imageSource + '&random=' + Math.random();
+            }, calculatedDelay);
+        }
+    }
+};
+
+var cameraApp = null;
+
+$(document).ready(function () {
+    cameraApp = new CameraApp();
+
+    $('#CameraImage').load(function() {
+        cameraApp.loadImage();
     });
-})(jQuery);
+
+    cameraApp.setup();
+});
